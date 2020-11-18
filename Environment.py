27cc858b0f3d561dcbe493950e5477dd5e71914e
Daniel <daniel.anthes97@gmail.com>
2020-11-18 16:39:13 +0100

    environment now resets itself when mouse reaches target

--- Environment.py
+++ Environment.py
@@ -17,6 +17,8 @@ class Maze:
         self.mousepos = np.array([0,1], dtype='float') # starting position of the mouse, north by default
         self.done = False # whether mouse has reached platform
         self.actions = np.array([[0,1], [1,1], [1,0], [1,-1], [0,-1], [-1,-1], [-1,0], [-1,1]], dtype='float') # mapping from action (index) to direction vector
+        self.starting_pos = np.array([[0,1],[1,0],[0,-1],[-1,0]], dtype='float') # possible starting locations
+        
         self.max_time = 120 # maximum trial duration in seconds
         self.time = 0
 
@@ -51,30 +53,32 @@ class Maze:
         # print(f"RECEIVED ACTION: {action}")
         self.time += self.timestep
         if self.done: # check whether simulation has ended
-            print("mouse has reached the platform, undefined behaviour")
-            return (None,None), -1, True
-        else:
-            direction = self.actions[action]
-            len_step = self.speed * self.timestep
-            delta_pos = (direction / np.sqrt(np.sum(direction**2))) * len_step
-            if not self._outOfBounds(self.mousepos + delta_pos): # if mouse would go out of bounds bounce back
-                self.mousepos += delta_pos
-
-            if self._outOfBounds(self.mousepos):
-                print("out of bounds!")
-
-
-            self.done = self._goalReached()
-            reward = 1 if self.done else 0
-
-            if not self.done and self.time > self.max_time: # if timelimit is exceeded and goal is not reached, stop without reward
-                self.done = True
-
-            doneval = 1 if self.done else 0
-
-            # pack return in an array
-            returnarr = np.array([self.mousepos[0], self.mousepos[1], reward, doneval, self.time])
+            print("mouse has reached the platform, resetting")
+            self.reset(self.starting_pos[np.random.choice(4),:]) # random starting position north, south, east, or west
+            returnarr = np.array([self.mousepos[0], self.mousepos[1], 0, 0, self.time])
             return returnarr
+
+        direction = self.actions[action]
+        len_step = self.speed * self.timestep
+        delta_pos = (direction / np.sqrt(np.sum(direction**2))) * len_step
+        if not self._outOfBounds(self.mousepos + delta_pos): # if mouse would go out of bounds bounce back
+            self.mousepos += delta_pos
+
+        if self._outOfBounds(self.mousepos):
+            print("out of bounds!")
+
+
+        self.done = self._goalReached()
+        reward = 1 if self.done else 0
+
+        if not self.done and self.time > self.max_time: # if timelimit is exceeded and goal is not reached, stop without reward
+            self.done = True
+
+        doneval = 1 if self.done else 0
+
+        # pack return in an array
+        returnarr = np.array([self.mousepos[0], self.mousepos[1], reward, doneval, self.time])
+        return returnarr
 
     def reset(self, mousepos=np.array([0,1], dtype='float')):
         '''
