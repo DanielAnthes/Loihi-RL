15bf9deb9f8a43f6cb010b4ee522829073dba036
Daniel <daniel.anthes97@gmail.com>
2020-11-18 11:23:47 +0100

    set up non learning model

--- Networks.py
+++ Networks.py
@@ -1,25 +1,27 @@
 import nengo
 import numpy as np
+from numpy.random import choice, random
 
 class ActorNet:
     '''
     TODO: INSERT INSIGHTFUL DESCRIPTION HERE
     '''
 
-def __init__(self, n_pc, n_neuron_in, n_neuron_out):
-    '''
-    Initialize actor net as a nengo network object
-    
-        PARAMS:
-            n_pc            -   number of place cells
-            n_neuron_in     -   number of neurons in Ensemble encoding input
-            n_neuron_out    -   number of neurons in Ensemble encoding output
-    '''
-    with nengo.Network() as net:
-        net.input = nengo.Ensemble(n_neurons=n_neuron_in, dimensions=n_pc, radius=np.sqrt(n_pc))
-        net.output = nengo.Ensemble(n_neurons=n_neuron_out, dimensions=8, radius=np.sqrt(8))
-        net.conn = nengo.Connection(net.input, net.output) # TODO: add learning here
-    self.net = net
+    def __init__(self, n_pc, n_neuron_in, n_neuron_out):
+        '''
+        Initialize actor net as a nengo network object
+        
+            PARAMS:
+                n_pc            -   number of place cells
+                n_neuron_in     -   number of neurons in Ensemble encoding input
+                n_neuron_out    -   number of neurons in Ensemble encoding output
+        '''
+        with nengo.Network() as net:
+            net.input = nengo.Ensemble(n_neurons=n_neuron_in, dimensions=n_pc, radius=np.sqrt(n_pc))
+            net.output = nengo.Ensemble(n_neurons=n_neuron_out, dimensions=8, radius=np.sqrt(8))
+            net.conn = nengo.Connection(net.input, net.output, function=lambda x: random(8))    # TODO: add learning here, 
+                                                                                                # TODO if something goes wrong here take a good long look at the initial connection function
+        self.net = net
 
 
 class CriticNet:
@@ -44,5 +46,90 @@ class CriticNet:
         with nengo.Network() as net:
             net.input = nengo.Ensemble(n_neurons=n_neuron_in, dimensions=n_pc, radius=np.sqrt(n_pc))
             net.output = nengo.Ensemble(n_neurons=n_neuron_out, dimensions=1)
-            net.conn = nengo.Connection(net.input, net.output) # TODO add learning here
+            net.conn = nengo.Connection(net.input, net.output, function=lambda x: random(1)) # TODO add learning here
         self.net = net
+
+
+class DecisionNode:
+    '''
+    TODO: combine with actor? model properly?
+    Node for use within a Nengo network.
+    Takes as input a vector of neural activities and
+    stochastically makes a choice for an action with probability proportional
+    to the relative activations of the neural input
+    '''    
+    def __init__(self, actions):
+        '''
+        INPUTS:
+            actions     -   a vector of possible actions to choose from
+        '''
+        self.actions = actions
+        with nengo.Network() as net:
+            net.choicenode = nengo.Node(lambda t,x: self.chooseAction(x), size_in=len(actions), size_out=1)
+        self.net = net
+
+    def chooseAction(self, activation_in):
+        '''
+        makes a random choice given neuronal activities for choices as input
+
+        INPUT:
+            activation_in   - neuronal acitivies (numpy array)
+        RETURNS:
+            decision    -   integer index of choice made
+        '''
+        probs = activation_in / np.sum(activation_in)
+        decision = choice(self.actions, p=probs)
+        return decision
+
+
+class PlaceCells:
+    '''
+    TODO: currently assumed "given" and implemented as Node, opportunity to extend the simulation here
+    Simple implementation of place cells as 2d gaussians uniformly distributed over a grid
+    activation is a function of the distance of the position to be encoded from the preferred location (mean) of the gaussian
+    max activation is 1
+    '''
+
+    def __init__(self, nx, ny, diameter, sigma):
+        '''
+        compute locations of place cells, for convenience, place cells are laid out on a square overlapping the arena
+        this means that some place cells lay outside of the arena. However, this ensures that place cells are evenly spaced
+        alternatively, initialize place cells given polar coordinates and arrange on concentric circles
+        but I do not know how to make them evenly spaced that way
+        for now the environment is assumed to be square and centered at (0,0)
+        
+        INPUTS:
+            nx          -   number of place cells in x direction
+            ny          -   number of place cells in y direction
+            diameter    -   diameter of the environment
+        '''
+        self.n_place_cells = nx * ny
+        self.sigma = sigma
+        x_coords = np.linspace(-diameter/2,diameter/2, nx)
+        y_coords = np.linspace(-diameter/2, diameter/2, ny)
+        xx, yy = np.meshgrid(x_coords, y_coords)
+        self.cell_locs = np.array([xx.flatten(), yy.flatten()])
+
+        with nengo.Network() as net:
+            net.placecells = nengo.Node(lambda t,x: self.place_cell_activation(x), size_in=2, size_out=self.n_place_cells)
+        
+        self.net = net
+
+    def place_cell_activation(self, pos):
+        '''
+        compute activity of all place cells for a given position,
+        firing rate for each place cell is given by a gaussian centered around it's preferred
+        location with width sigma
+        returns a vector of activation of all place cells
+
+        INPUTS:
+            pos         -   position to be encoded
+        
+        RETURNS:
+            activations -   vector encoding activities of all place cells
+        '''    
+        pos = np.expand_dims(pos,1)
+        enumerator = np.sqrt(np.sum( (pos - self.cell_locs)**2 ,axis=0))**2
+        denominator = 2 * self.sigma**2
+        activations = np.exp(-(enumerator / denominator))
+        return activations
--- run.py
+++ run.py
@@ -1,4 +1,32 @@
+import nengo
+
 from Environment import Maze
+from Agent import Mouse
 
 
-env = Maze()
+
+
+# set up simulation, connect networks
+with nengo.Network() as model:
+    env = Maze()
+    agent = Mouse(env, 23, 23, act_lr=0.01, crit_lr=0.01)
+
+    # TODO add error node
+    # environment node, step function expects integer so need to cast from float
+    envstate = nengo.Node(lambda time, action: env.step(int(action)), size_in=1, size_out=5)
+    
+    # compute place cell activations
+    nengo.Connection(envstate[:2], agent.PlaceCells.net.placecells)
+    
+    # place cells give input to actor and critic
+    nengo.Connection(agent.PlaceCells.net.placecells, agent.Critic.net.input)
+    nengo.Connection(agent.PlaceCells.net.placecells, agent.Actor.net.input)
+
+    # take actor net as input to decision node
+    nengo.Connection(agent.Actor.net.output, agent.DecisionMaker.net.choicenode)
+
+    # execute action in environment
+    nengo.Connection(agent.DecisionMaker.net.choicenode, envstate)
+
+with nengo.Simulator(model) as sim:
+    sim.run(10)
