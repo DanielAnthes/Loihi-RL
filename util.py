2832d17e28060d20e785a181a6b524dce9360a9c
Fabian Schneider <fabi.schn@gmail.com>
2020-11-20 11:02:11 +0100

    trajectory plots updated

--- util.py
+++ util.py
@@ -22,39 +22,31 @@ def plot_sim(sim, envprobe, errorprobe):
 
 def plot_trajectories(sim, env, envprobe):
     '''
-    plot trajectories
-    TODO: known to break
+    trajectory plots
     '''
-    
     episode_indices = np.where(sim.data[envprobe][:,3] == 1.0)
     episode_indices = np.append(episode_indices[0], max(sim.trange()) / env.timestep)
 
-    plot_columns = 2
-    plot_rows = ceil(len(episode_indices) / plot_columns)
+    fig = plt.figure()
+    ax = plt.gca()
 
-    prior = 0
-    n = 1
+    last_episode = 0
+    for episode in episode_indices:
+        vx = sim.data[envprobe][int(last_episode):int(episode),0]
+        vy = sim.data[envprobe][int(last_episode):int(episode),1]
+        colour = np.random.random(3)
+        ax.plot(vx, vy, '-', alpha=0.6, c=colour, label="%d-%d" % (int(last_episode), int(episode))) # plot all points
+        ax.plot(vx[0], vy[0], 'o', alpha=0.6, c=colour) # plot start point as o
+        ax.plot(vx[-1], vy[-1], 'x', alpha=0.6, c=colour) # plot end point as x
+        last_episode = episode + 1
 
-    plt.figure()
-
-    for index in episode_indices:
-        vx = sim.data[envprobe][int(prior):int(index),0]
-        vy = sim.data[envprobe][int(prior):int(index),1]
-        sub = int(plot_rows * 100 + plot_columns * 10 + n)
-
-        ax = plt.subplot(sub)
-        ax.plot(vx, vy, ".-")
-        arena = plt.Circle((0,0), 1, color='k', fill=False)
-        platform = plt.Circle(env.platform_loc, env.platformsize/2, fill=True, color='k')
-        ax.add_artist(platform)
-        ax.add_artist(arena)
-        ax = plt.gca()
-        ax.axis('equal')
-        plt.xlim([-1.5, 1.5])
-        plt.ylim([-1.5, 1.5])
-        plt.title("%d-%dms" % (int(prior), int(index)))
-
-        prior = index + 1
-        n += 1
-
+    arena = plt.Circle((0,0), 1, color='k', fill=False)
+    platform = plt.Circle(env.platform_loc, env.platformsize/2, fill=True, color='k')
+    ax.add_artist(platform)
+    ax.add_artist(arena)
+    ax.axis('equal')
+    ax.legend()
+    plt.xlim([-1.5, 1.5])
+    plt.ylim([-1.5, 1.5])
+    plt.title("trajectory")
     plt.show()
