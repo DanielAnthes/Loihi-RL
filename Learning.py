dea3638f91f8469ae0497ed0607bf47278708787
Fabian Schneider <fabi.schn@gmail.com>
2020-12-03 21:21:06 +0100

    Removed bouncing + added TDL documentation

--- Learning.py
+++ Learning.py
@@ -29,49 +29,73 @@ import numpy as np
 
 class TDL(LearningRuleType):
     '''
-    Temporal difference learning rule
-    Class for nengo.learning_rules
+    Temporal difference learning constructor.
+    Initialises the learning rule for nengo.learning_rules such that Learning.TDL()
+    becomes a recognised callable and sets its a default configuration.
     '''
 
     modifies = 'weights'
     probeable = ('pre_filtered', 'post_filtered', 'delta')
 
-    learning_rate = NumberParam(
-        'learning_rate', low=0, readonly=True, default=1e-6
-    )
-    pre_synapse = SynapseParam(
-        'pre_synapse', default=Lowpass(tau=0.005), readonly=True
-    )
-    post_synapse = SynapseParam(
-        'post_synapse', default=None, readonly=True
-    )
-    beta = NumberParam('beta', low=0, readonly=True, default=1.0)
+    learning_rate = NumberParam('learning_rate', low=0, readonly=True, default=1e-6)
+    pre_synapse = SynapseParam('pre_synapse', default=Lowpass(tau=0.005), readonly=True)
+    post_synapse = SynapseParam('post_synapse', default=None, readonly=True)
 
-    def __init__(self, learning_rate=Default, pre_synapse=Default, post_synapse=Default, beta=Default):
+    def __init__(self, learning_rate=Default, pre_synapse=Default, post_synapse=Default):
+        '''
+        INPUTS:
+            learning_rate   -   Learning rate for connection, 1e-6 by default
+            pre_synapse     -   PreSynaptic neuron population
+            post_synapse    -   PostSynaptic neuron population
+        NOTE:
+            pre_synapse and post_synapse must be of type Ensemble if LstsqL2
+            is employed as specified solver for weights
+        '''
+
         super(TDL, self).__init__(learning_rate, size_in=1)
 
-        self.beta = beta
         self.pre_synapse = pre_synapse
-        self.post_synapse = (self.pre_synapse if post_synapse is Default else post_synapse)
+        self.post_synapse = (self.pre_synapse if post_synapse is Default else post_synapse)  # if no post_synapse is supplied, recurrent connection is assumed
 
     @property
     def _argdefaults(self):
         return (('learning_rate', TDL.learning_rate.default),
                 ('pre_synapse', TDL.pre_synapse.default),
-                ('post_synapse', TDL.post_synapse.default),
-                ('beta', TDL.beta.default))
+                ('post_synapse', TDL.post_synapse.default))
 
 class SimTDL(Operator):
     '''
-    Temporal difference learning rule
-    Class for nengo.builder.learning_rules
+    Simulator object for temporal difference learning on CPU.
+    This is the class used after simulation steps to compute weight changes that
+    are implemented as:
+
+        d_ij = α𝛿(PreSynapticActivity * PostSynapticActivity)
+
+        where:
+
+            α = LearningRate * dt
+            𝛿 = PredictionError
+
+    Delta has to be a single float value that is fed into the connection (e.g.,
+    nengo.Connection(my_error_ensemble, my_tdl.conn.learning_rule).
     '''
 
-    def __init__(self, pre_filtered, post_filtered, weights, error, delta, learning_rate, beta, encoders=None, tag=None):
-        super(SimTDL, self).__init__(tag=tag)
-        self.learning_rate = learning_rate
-        self.beta = beta
+    def __init__(self, pre_filtered, post_filtered, weights, error, delta, learning_rate, encoders=None, tag=None):
+        '''
+        INPUTS:
+            pre_filtered    -   Filtered activity from presynaptic neuron(s)
+            post_filtered   -   Filtered activity from postsynaptic neuron(s)
+            weights         -   Weight matrix
+            error           -   Prediction error 𝛿 (single-valued float)
+            delta           -   Weight updates
+            learning_rate   -   Learning rate
+            encoders        -   Encoders
+            tag             -   Connection tag
+        '''
 
+        super(SimTDL, self).__init__(tag=tag)
+
+        self.learning_rate = learning_rate
         self.sets = []
         self.incs = []
         self.reads = [pre_filtered, post_filtered, weights, error] + ([] if encoders is None else [encoders])
@@ -102,9 +126,17 @@ class SimTDL(Operator):
         return None if len(self.reads) < 5 else self.reads[4]
 
     def _descstr(self):
+        '''
+        Returns nengo description string.
+        '''
         return 'pre=%s, post=%s -> %s' % (self.pre_filtered, self.post_filtered, self.delta)
 
     def make_step(self, signals, dt, rng):
+        '''
+        Returns the actual function that computes the weight changes
+        to nengo.
+        '''
+
         weights = signals[self.weights]
         pre_filtered = signals[self.pre_filtered]
         post_filtered = signals[self.post_filtered]
@@ -112,13 +144,19 @@ class SimTDL(Operator):
         delta = signals[self.delta]
 
         def step_simtdl():
-            delta[...] = self.learning_rate * dt * error * np.outer(post_filtered, pre_filtered)
+            delta[...] = self.learning_rate * dt * error * np.outer(post_filtered, pre_filtered)  # adjust formula here
         return step_simtdl
 
 @Builder.register(TDL)
 def build_tdl(model, tdl, rule):
     '''
-    Build the .BCM object into our model
+    Registers a .BCM object in model for nengo builder such that
+    simulator knows what class to use for weight computations.
+
+    INPUTS:
+        model   -   Model wherein operation is added
+        tdl     -   TDL() connection object for which rule is implemented
+        rule    -   Nengo identification string for outputs to be read in SimTDL
     '''
 
     conn = rule.connection
@@ -139,16 +177,33 @@ def build_tdl(model, tdl, rule):
                         error,
                         model.sig[rule]['delta'],
                         learning_rate=tdl.learning_rate,
-                        beta=tdl.beta,
                         encoders=encoders))
 
     model.sig[rule]['pre_filtered'] = pre_filtered
     model.sig[rule]['post_filtered'] = post_filtered
     model.sig[rule]['error'] = error
 
-def plan_tdl(queue, pre, post, weights, errors, delta, alpha, beta, encoders=None, tag=None):
+def plan_tdl(queue, pre, post, weights, errors, delta, alpha, encoders=None, tag=None):
+    '''
+    Full planning function for temporal difference learning on GPU.
+    C implementation of SimTDL. Please refer to SimTDL for formulation of
+    the learning rule applied.
+
+    INPUTS:
+        pre         -   Filtered activity from presynaptic neuron(s)
+        post        -   Filtered activity from postsynaptic neuron(s)
+        weights     -   Weight matrix
+        errors      -   Prediction error 𝛿 (single-valued float)
+        delta       -   Weight updates
+        alpha       -   Effective learning rate (α = LearningRate * dt)
+        encoders    -   Encoders
+        tag         -   Connection tag
+    OUTPUTS:
+        plan        -   Plan for GPU
+    '''
+
     assert (
-        len(pre) == len(post) == len(weights) == len(delta) == alpha.size == beta.size
+        len(pre) == len(post) == len(weights) == len(delta) == alpha.size
     )
     N = len(pre)
 
@@ -168,8 +223,14 @@ def plan_tdl(queue, pre, post, weights, errors, delta, alpha, beta, encoders=Non
         == weights.ctype
         == delta.ctype
         == alpha.ctype
-        == beta.ctype
     )
+
+    '''
+    text = function to be executed on GPU. This is the equivalent
+    of SimTDL::make_step::step_simtdl() that computes weight the
+    weight changes. In the case of the GPU, weight changes are
+    computed in strides between pre- and postsynaptic activity.
+    '''
 
     text = """
     __kernel void tdl(
@@ -188,8 +249,7 @@ def plan_tdl(queue, pre, post, weights, errors, delta, alpha, beta, encoders=Non
         __global const int *delta_stride0s,
         __global const int *delta_starts,
         __global ${type} *delta_data,
-        __global const ${type} *alphas,
-        __global const ${type} *betas
+        __global const ${type} *alphas
     )
     {
         const int ij = get_global_id(0);
@@ -205,7 +265,7 @@ def plan_tdl(queue, pre, post, weights, errors, delta, alpha, beta, encoders=Non
             weights_starts[k] + i*weights_stride0s[k] + j];
         const ${type} error = errors[sizeof(errors) / sizeof(errors[0])];
         const ${type} alpha = alphas[k];
-        const ${type} beta = betas[k];
+
         if (i < shape0) {
             delta[i*delta_stride0s[k] + j] =
                 alpha * error * (pre * post);
@@ -233,7 +293,6 @@ def plan_tdl(queue, pre, post, weights, errors, delta, alpha, beta, encoders=Non
         delta.cl_starts,
         delta.cl_buf,
         alpha,
-        beta,
     )
     if GPU:
         _fn = cl.Program(queue.context, text).build().tdl
@@ -251,19 +310,28 @@ def plan_tdl(queue, pre, post, weights, errors, delta, alpha, beta, encoders=Non
         + errors.nbytes
         + delta.nbytes
         + alpha.nbytes
-        + beta.nbytes
     )
     return plan
 
 def plan_SimTDL(self, ops):
+    '''
+    SimTDL planner for GPU. Effectively, this retrieves all
+    the relevant data out of the simulator at the current
+    timestep and passes it on to our TDL planner.
+
+    INPUTS:
+        ops     -   Nengo operations (supplied by nengo)
+    OUTPUTS:
+        plan    -   Plan for GPU
+    '''
+
     pre = self.all_data[[self.sidx[op.pre_filtered] for op in ops]]
     post = self.all_data[[self.sidx[op.post_filtered] for op in ops]]
     weights = self.all_data[[self.sidx[op.weights] for op in ops]]
     error = self.all_data[[self.sidx[op.error] for op in ops]]
     delta = self.all_data[[self.sidx[op.delta] for op in ops]]
     alpha = self.Array([op.learning_rate * self.model.dt for op in ops])
-    beta = self.Array([op.beta for op in ops])
-    return [plan_tdl(self.queue, pre, post, weights, error, delta, alpha, beta)]
+    return [plan_tdl(self.queue, pre, post, weights, error, delta, alpha)]
 
 if GPU:
-    setattr(nengo_ocl.Simulator, 'plan_SimTDL', plan_SimTDL)
+    setattr(nengo_ocl.Simulator, 'plan_SimTDL', plan_SimTDL)  # Register GPU callable for our learning rule with nengo_ocl simulator if libraries are available
