b28081f2e4df493e90981cd7164df2b9d245b384
DanielAnthes <daniel.anthes97@gmail.com>
2020-12-29 03:24:47 -0800

    add loihi test to run on chip

--- loihitest.py
+++ loihitest.py
@@ -2,10 +2,11 @@ import nengo
 import nengo_loihi
 
 with nengo.Network() as net:
-    net.ens = nengo.Ensemble(n_neurons=100, dimensions=1)
-    
+    nengo_loihi.add_params(net)
+    ens = nengo.Ensemble(n_neurons=100, dimensions=1)
+    net.config[ens].on_chip = True
 
-with nengo.Simulator(net) as sim:
+with nengo_loihi.Simulator(net) as sim:
     sim.run(30)
 
 print("done")
